231ca480d28c7fc14333408a34dc38d33c17d913
Daniel Rocha <rochadaniel15@gmail.com>
2021-06-24 17:38:56 -0300

    add: Adicionado estilo para a pagina de login de exemplo

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,14 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ config('app.name')}} - @yield('title') </title>
-    <link rel="stylesheet" href='{{ asset("asset/css/reset.css") }}'/>
     <link rel="stylesheet" href='{{ asset("asset/css/base.css") }}'/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <section class='container'>
         <header class='base-header'>
             <img class='logo'src='{{ asset("asset/img/logo.png") }}' alt='logo' />
-            <h1> {{ config('app.name') }}</h1>
+            <h1 class='fs-1"'> {{ config('app.name') }}</h1>
         </header>
 
         <main class='base-content'>
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -2,39 +2,47 @@
 
 @section('title', 'Registro de Usuario')
 @section('content')
-<section>
-    <h2>Cadastro</h2>
-    <form class="form-register" action="{{route('save')}}" method="post">
-        {{ csrf_field() }}
-        <label>
-            <span>Nome</span>
-            <input type="text" name="name" />
+<section class="d-flex justify-content-center">
 
+    <form class='container-sm col col-lg-4 custom-form' action="{{route('save')}}" method="post">
+        <h1>Cadastro</h1>
+        {{ csrf_field() }}
+        <div class="mb-3">
+            <label class="col-form-label"> Nome </label>
+            <input class="form-control" type="text" name="name"/>
             @if($errors->has('name'))
                 @foreach($errors->get('name') as $erro)
-                    <strong>{{ $erro }}</strong>
+                    <div class="alert alert-danger" role="alert" style='padding: 0 5px;'>
+                        {{ $erro }}
+                    </div>
                 @endforeach
             @endif
-        </label>
-        <label>
-            <span>E-mail</span>
-            <input type="text" name="email" />
+        </div>
+        <div class="mb-3">
+            <label class="col-form-label">E-mail</label>
+            <input class="form-control" type="text" name="email" />
             @if($errors->has('email'))
                 @foreach($errors->get('email') as $erro)
-                    <strong>{{ $erro }}</strong>
+                    <div class="alert alert-danger" role="alert" style='padding: 0 5px;'>
+                        {{ $erro }}
+                    </div>
                 @endforeach
             @endif
-        </label>
-        <label>
-            <span>Senha</span>
-            <input type="password" name="password" />
+        </div>
+        <div class="mb-3">
+            <label class="col-form-label">Senha</label>
+            <input class="form-control" type="password" name="password" />
             @if($errors->has('password'))
                 @foreach($errors->get('password') as $erro)
-                    <strong>{{ $erro }}</strong>
+                    <div class="alert alert-danger" role="alert" style='padding: 0 5px;'>
+                        {{ $erro }}
+                    </div>
                 @endforeach
             @endif
-        </label>
-        <input type="submit" value="Enviar" />
+        </div>
+        <div class="d-grid gap-2">
+            <button class="btn btn-outline-dark" type="submit">Enviar</button>
+        </div>
     </form>
     <link rel="{{ asset('asset/css/register/form.css')}}" href="style.css">
 </section>
